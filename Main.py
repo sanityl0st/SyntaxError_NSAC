ae68a09b3b3fda69c536df17d87de2bdba60dbeb
sanityl0st <146678570+sanityl0st@users.noreply.github.com>
2023-10-06 18:24:25 -0700

    Added Python file for getting data using numerous APIs. Currently, only the ATTAIN API is in use

---
+++ Main.py
@@ -0,0 +1,64 @@
+
+#importing requests library so that we can get requests from API
+import requests
+
+#importing json library so that we can format JSON to Python Dictionary 
+import json
+
+
+#---------------------ATTAIN API USAGE--------------------------
+
+#Made a list of all U.S. Abbreviation in case we decide to use a dropdown
+stateAbbreviation = (["AL","AK","AZ","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA",
+                      "ME","MD","MA","MI","MN","MS","MO","MT","NE","NH","NV","NJ","NM","NY","ND","NC","OH",
+                      "OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"])
+
+#This function works by taking the user input for their state abbreviation and finding out its orgCode
+def searchStateCode(stateAB):
+    
+    #Error Checking to see if user inputted something longer than 2
+    if (len(stateAB) > 2):
+        #Error Message
+        return "Error Message: Invalid State Abbreivation. Please enter a valid State Abbreviation"
+
+    #This is the url for the attain api to search for OrgStateCodes, needed to data on that State
+    baseURL = "https://attains.epa.gov/attains-public/api/domains?domainName=OrgStateCode"
+    #Pulls required info from baseURL
+    info = requests.get(baseURL)
+
+    #Formats it to json
+    orgCodeData = info.json()
+    stateCode = ""
+    #Searchs for user input of state and stores its orgCode if found
+    for i in orgCodeData:
+        if (stateAB in i["name"]):
+            if ( i["context2"] == "State"):
+
+                stateCode = i["context"]
+                print(stateCode)
+    #Error testing to make sure state code has a value
+    if (stateCode != ""):
+        return stateCode
+    else:
+        return "Error Message: State not found"
+
+#
+def StateSummary(stateAB):
+    #pulls state code from previous function
+    stateCode = searchStateCode(stateAB)
+
+    #URL for summary of the State Data
+    baseURL = "https://attains.epa.gov/attains-public/api/usesStateSummary?organizationId="
+    url = baseURL + stateCode
+    info = requests.get(url)
+    data = info.json()
+
+    #Just prints the data. Still developing which data values will be of most use for project
+    print(data)
+
+
+#Test Cases for California
+searchStateCode("CA")
+StateSummary("CA")
+
+    
