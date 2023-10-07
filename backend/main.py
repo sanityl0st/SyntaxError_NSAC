385d2a8b1a05599acdaddad10dfe5acc792d413c
aelew <118043566+aelew@users.noreply.github.com>
2023-10-07 15:00:58 -0700

    use underscores instead of spaces

--- backend/main.py
+++ backend/main.py
@@ -110,7 +110,7 @@ def run():
     county = request.args.get('county')
     AUI = getAUI(stateID, county)
     wInfo = getWaterInfo(stateID, AUI[1][0])
-    return {"body of water" : AUI[0][0], "info" : wInfo}
+    return {"body_of_water" : AUI[0][0], "info" : wInfo}
 
 #Test Cases for California
 #searchStateCode("CA")
